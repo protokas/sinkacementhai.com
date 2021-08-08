3af3b7f00899f50210de43db3793d12774193dbb
SineWave <protokas64@gmail.com>
2021-08-08 15:59:21 +0700

    commit

--- index.php
+++ index.php
@@ -460,7 +460,6 @@
 
     <!--  Bootstrap HTML  -->
 
-    <div class="container">
         <div id="lawmsg" class="alert alert-info alert-dismissible h6 fade show fixed-bottom " role="alert">
             <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
@@ -468,8 +467,6 @@
             &nbsp; We use cookies on this website to distinguish you from other users. &nbsp; We use this data to enhance your experience and for targeted advertising. &nbsp; By continuing to use this website you consent to our use of cookies. &nbsp; For more information, please see our &nbsp;
             <a href="https://policies.google.com/?hl=en-US" target="_blank">Cookie Policy</a>.
         </div>
-    </div>
-
 
     <!-- Footer-->
     <!-- Bootstrap core JS-->
