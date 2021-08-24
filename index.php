8972c819b44eecfed909da446d4a68e16f317736
SineWave <protokas64@gmail.com>
2021-08-24 08:18:38 +0700

    commit

--- index.php
+++ index.php
@@ -65,6 +65,14 @@
         }(document, 'script', 'facebook-jssdk'));
     </script>
 
+    <!-- The core Firebase JS SDK is always required and must be listed first -->
+    <script src="/__/firebase/8.10.0/firebase-app.js"></script>
+
+    <!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+
+    <!-- Initialize Firebase -->
+    <script src="/__/firebase/init.js"></script>
     <!-- Navigation-->
     <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
         <div class="container">
