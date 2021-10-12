1ba4b28e61e619bb3a29b0ab1f77181b21f2d72c
SineWave <protokas64@gmail.com>
2021-10-12 10:18:39 +0700

    commit

--- index.php
+++ index.php
@@ -118,7 +118,7 @@
         <div class="container">
             <!-- Featured Project Row-->
             <div class="row align-items-center no-gutters mb-4 mb-lg-5">
-                <div class="col-xl-8 col-lg-7"><img class="img-fluid mb-3 mb-lg-0" src="img/slid/roof.jpg" alt="" />
+                <div class="col-xl-8 col-lg-7"><img class="img-fluid mb-3 mb-lg-0" src="imgorigin/pic4.jpg" alt="" />
                 </div>
                 <div class="col-xl-4 col-lg-5">
                     <div class="featured-text text-center text-lg-left">
@@ -224,7 +224,7 @@
                 </ul>
             </div>
         </article>
-        
+
     </main>
     <br>
     <br>
